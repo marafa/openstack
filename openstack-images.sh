c8e9ac44cee07ed9086fd543c646709c09aea237
root <root@compute02.marafa.vm>
2013-09-14 23:50:37 -0400

    initial commit

---
+++ openstack-images.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+#downloads images, checks md5sum, adds the image to glance
+
+dir=/root/images.tmp
+
+if [ $# -eq 0 ]
+then
+	echo " Usage: `basename $0` all centos cirros fedora"
+	exit 1
+fi
+
+cirros(){
+	echo cirros
+	image=cirros-0.3.0-x86_64-disk.img
+	location=https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img
+	name="CirrOS 0.3.0"
+	images
+}
+
+fedora(){
+	echo fedora
+	location=http://download.fedoraproject.org/pub/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2
+	image=Fedora-x86_64-19-20130627-sda.qcow2
+	name="Fedora 19 x86_64"
+	images
+}
+
+centos(){
+	echo centos
+	location=http://mirror.catn.com/pub/catn/images/qcow2/centos6.4-x86_64-gold-master.img
+	image=centos6.4-x86_64-gold-master.img 
+	name="CentOS 6.4 x86_64"
+	images
+}
+
+images(){
+echo $image from $location with $name
+
+wget $location
+md5sum -c glance-images.md5sum  > file.tmp
+grep $image file.tmp | grep OK > /dev/null
+rm -rf file.tmp
+if [ $? -eq 0 ]
+then
+	. /root/keystonerc_admin
+	glance image-create --name '$name' --disk-format qcow2 --container-format bare --is-public true < $dir/$image
+else
+	echo " ERROR: Image md5um indicates $image is corrupt"
+fi
+}
+
+all(){
+	cirros
+	fedora
+	centos
+}
+
+if ! [ -d $dir ]
+then
+	mkdir -p /root/$dir
+fi
+
+cd $dir
+
+if [ -f $dir/glance-images.md5sum ]
+then
+	wget http://github.com/marafa/glance-images.tmp
+fi
+
+case $1 in
+	cirros)
+		cirros
+	;;
+	centos)
+		centos
+	;;
+	fedora)
+		fedora
+	;;
+	all)
+		all
+	;;
+esac
+
+#cleanup
+cd -
---
+++ openstack-logs.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+version=0.2
+
+if ! [ -f /usr/bin/multitail ]
+then
+	echo  ERROR: multitail not found!
+	yum install multitail
+else
+#	multitail /var/log/libvirt/libvirtd.log /var/log/nova/*log /var/log/messages
+	echo `basename $0` $version
+fi
+
+if [ $# -eq 0 ]
+then
+	echo " Usage: `basename $0` component"
+	echo "	Where component is one of nova, keystone, neutron, openvswitch, horizon, ceilometer, cinder, glance, foreman, puppet, system"
+	exit 2
+fi
+
+case $1 in
+	nova)
+		multitail /var/log/nova/*log
+	;;
+	keystone)
+		multitail /var/log/keystone/*log
+	;;
+	neutron)
+		multitail /var/log/neutron/*log
+	;;
+	openvswitch)
+		multitail /var/log/openvswitch/*log
+	;;
+	horizon)
+		multitail /var/log/horizon/*log
+	;;
+	ceilometer)
+		multitail /var/log/ceilometer/*log
+	;;
+	cinder)
+		multitail /var/log/cinder/*log
+	;;
+	glance)
+		multitail /var/log/glance/*log
+	;;
+	foreman)
+                multitail /var/log/foreman/*log
+	;;
+	puppet)
+                multitail /var/log/puppet/*log
+	;;
+	system)
+		multitail /var/log/{messages,secure}
+	;;
+esac
