867f3688e4f8d1034f72b2db3ceb0270a226bca4
marafa <root@compute02.marafa.vm>
2013-09-17 21:04:32 -0400

    initial commit

---
+++ openstack-os-tools.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+config_vnstat(){
+if [ -f /usr/bin/vnstat ] 
+then
+        for int in `vnstat --iflist| sed 's/Available interfaces://'` 
+        do 
+                echo " INFO: Initialising vnstat db for $int"
+                vnstat -u -i $int
+        done
+
+        chown vnstat.vnstat  `cat /etc/vnstat.conf | grep DatabaseDir |sed 's/"//g' |awk '{print $2 "/*"}'`
+
+	chkconfig vnstat on
+        service vnstat restart
+fi
+}
+
+install_tools(){
+yum install htop iotop byobu screen alpine vnstat
+}
+
+install_tools
+config_vnstat
