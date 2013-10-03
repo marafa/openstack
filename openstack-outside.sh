70b7a27283a1144f08ae1b768070d700481358b7
root <root@compute01.marafa.vm>
2013-10-03 07:48:17 -0400

    works now without much error checking

--- openstack-outside.sh
+++ openstack-outside.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. /root/keystonerc_admin
+
 device=eth0
 vlan=192.168.0
 start=$vlan.129
@@ -7,29 +9,30 @@ end=$vlan.163
 gw=$vlan.1
 dns1=4.2.2.2
 dns2=
-hwaddr=`cat /etc/udev/rules.d/70-persistent-net.rules | grep $device | cut -d, -f4 | sed 's/ ATTR{address}=="0//g' | sed 's/"//g'`
+hwaddr=`cat /etc/udev/rules.d/70-persistent-net.rules | grep $device | cut -d, -f4 | sed 's/ ATTR{address}=="//g' | sed 's/"//g'`
+IP=$vlan.31
 
-###before
-ifconfig > /tmp/before.log
-ip netns >> /tmp/before.log
 
+public(){
 neutron router-create PublicRouter
 neutron net-create PublicLAN --router:external=True
-neutron subnet-create PublicLAN $vlan/24 --name PublicLAN --enable_dhcp=False --allocation-pool start=$start,end=$start --gateway=$gw --dns_nameservers list=true $dns1 $dns2
+neutron subnet-create PublicLAN $vlan.0/24 --name PublicLAN --enable_dhcp=False --allocation-pool start=$start,end=$start --gateway=$gw --dns_nameservers list=true $dns1 $dns2
 neutron router-gateway-set PublicRouter PublicLAN
+}
 
 ##determine one physical nic or more
-
 ##if one nic move ip from nic to br-ex in /etc/sysconfig/network-scripts
-
+device_exist(){
 ifconfig $device > /dev/null  2>&1
 if ! [ $? -eq 0 ]
 then
 	echo " ERROR: $device not found"
 	exit 1
 fi
+}
 
-cp /etc/sysconfig/network-scripts/ifcfg-$device /root/ifcfg-$device
+device_primary(){
+mv /etc/sysconfig/network-scripts/ifcfg-$device /root/ifcfg-$device
 cat >> /etc/sysconfig/network-scripts/ifcfg-$device << EOF
 DEVICE=$device
 HWADDR=$hwaddr
@@ -38,8 +41,9 @@ DEVICETYPE=ovs
 OVS_BRIDGE=br-ex
 ONBOOT=yes
 EOF
+}
 
-###after
+device_bridge(){
 cat >> /etc/sysconfig/network-scripts/ifcfg-br-ex << EOF
 DEVICE=br-ex
 DEVICETYPE=ovs
@@ -53,6 +57,10 @@ DNS2=$dns2
 DNS3=$dns3
 ONBOOT=yes
 EOF
+}
 
-ifconfig > /tmp/after.log
-ip netns >> /tmp/after.log
+###MAIN
+device_exist
+device_primary
+device_bridge
+public
