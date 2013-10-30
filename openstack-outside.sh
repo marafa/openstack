d31091c1d60c31ac3f927b68a6c5c94a9e80f737
root <root@compute03.marafa.vm>
2013-10-30 00:59:59 -0400

    cleanup; works now too

--- openstack-outside.sh
+++ openstack-outside.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-. /root/keystonerc_admin
+source /root/keystonerc_admin
 
 device=eth0
 vlan=192.168.0
-start=$vlan.129
-end=$vlan.163
+start=$vlan.150
+end=$vlan.200
 gw=$vlan.1
 dns1=4.2.2.2
 dns2=
+dns3=
 hwaddr=`cat /etc/udev/rules.d/70-persistent-net.rules | grep $device | cut -d, -f4 | sed 's/ ATTR{address}=="//g' | sed 's/"//g'`
 IP=`ifconfig $device|grep -w inet|awk '{print $2}'|cut -d: -f2`
-
-echo debug $IP
+domain=marafa.vm
 
 public(){
 neutron router-create PublicRouter
-neutron net-create PublicLAN --router:external=True
-neutron subnet-create PublicLAN $vlan.0/24 --name PublicLAN --enable_dhcp=False --allocation-pool start=$start,end=$end --gateway=$gw --dns_nameservers list=true $dns1 $dns2
+neutron net-create --tenant-id services PublicLAN --router:external=True
+neutron subnet-create --tenant-id services --allocation-pool start=$start,end=$end --gateway=$gw --disable-dhcp --name PublicSubnet PublicLAN $vlan.0/24
 neutron router-gateway-set PublicRouter PublicLAN
 }
 
@@ -41,26 +41,28 @@ device_primary(){
 cat >> /etc/sysconfig/network-scripts/ifcfg-$device << EOF
 DEVICE=$device
 HWADDR=$hwaddr
-TYPE=OVSPort
-DEVICETYPE=ovs
-OVS_BRIDGE=br-ex
 ONBOOT=yes
+#TYPE=OVSPort
+#DEVICETYPE=ovs
+#OVS_BRIDGE=br-ex
 EOF
 }
 
 device_bridge(){
 cat >> /etc/sysconfig/network-scripts/ifcfg-br-ex << EOF
 DEVICE=br-ex
-DEVICETYPE=ovs
-TYPE=OVSBridge
-BOOTPROTO=static
 IPADDR=$IP
-NETMASK=255.255.255.0 
+PREFIX=24
 GATEWAY=$gw 
 DNS1=$dns1
 DNS2=$dns2
 DNS3=$dns3
+SEARCH=$domain
 ONBOOT=yes
+#DEVICETYPE=ovs
+#TYPE=OVSBridge
+#BOOTPROTO=static
+#NETMASK=255.255.255.0 
 EOF
 }
 
@@ -72,7 +74,7 @@ ovs(){ #open vswitch
 ovs-vsctl add-port br-ex $device; service network restart
 }
 
- ###MAIN
+###MAIN
 device_exist
 backup
 device_primary
