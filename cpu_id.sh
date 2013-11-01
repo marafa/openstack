ecc97d8d10be95098db5d0f9fdbe6d7f04e8ae5c
root <root@compute01.marafa.vm>
2013-10-31 21:02:00 -0400

    initial commit

---
+++ cpu_id.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#does the cpu support virtualisation?
+grep -E "(vmx|svm)" --color=always /proc/cpuinfo > /dev/null
+if ! [ $? -eq 0 ]
+then
+	echo " WARN: Your cpu does not support Virtualisation"
+else
+	echo " INFO: Your cpu supports Virtualisation"
+	#is virtualisation enabled in the bios
+	lsmod | grep kvm_intel > /dev/null
+	if ! [ $? -eq 0 ]
+	then
+		echo " WARN: Enable virtualisation in your BIOS"
+	else
+		#is nested kvm supported?
+		modinfo kvm_intel | grep -i nested > /dev/null
+		if ! [ $? -eq 0 ]
+		then
+			echo " WARN: Your cpu does not support nested kvm"
+		else
+			echo " INFO: Your cpu supports nested kvm"
+			if [ $UID -eq 0 ] 
+			then 
+				enabled=`cat /sys/module/kvm_intel/parameters/nested `
+				if [ "$enabled" == "Y" ]
+				then
+					echo " INFO: Nothing to do. Nested KVM is enabled"
+					exit 0
+				else
+					echo " INFO: Nested KVM is not enabled. Enabling"
+					if ! [ -f /etc/modprobe.d/nested_kvm.conf ]
+					then
+						echo "options kvm-intel nested=y" > /etc/modprobe.d/nested_kvm.conf
+					fi
+					echo " INFO: Please reboot to enable nested kvm"
+				fi
+			else
+				echo " WARN: You need to be root to enable nested kvm"
+			fi
+		fi
+	fi
+fi
