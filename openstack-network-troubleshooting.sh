32df91eb569fbbda81b41460d8a946f11916c448
root <root@compute.marafa.vm>
2013-11-05 07:30:50 -0500

    added ovs and openstack status; cleaner output of ifcfg files

--- openstack-network-troubleshooting.sh
+++ openstack-network-troubleshooting.sh
@@ -1,19 +1,35 @@
 #!/bin/sh
 
+#this is a script that collects information to aid in troubleshooting efforts
+
 source /root/keystonerc_admin
 alias quantum=neutron
 
+echo "---Packages Installed---"
 rpm -qa | grep -iE "neutron|nova-network|openstack*" | sort
 
+for i in `ls /etc/sysconfig/network-scripts/ifcfg-*`
+do
+	echo "---Device config for $i---"
+	cat $i
+done
+echo "---Ifconfig---"
 ifconfig
+echo "---Routes----"
 route -n
-ls -ltr /etc/sysconfig/network-scripts/ifcfg-*
-
+echo ---Open vSwitch---
+ovs-vsctl show
+echo ---Network Namespaces---
 ip netns
 
+echo ---Neutron devices---
 neutron router-list
 neutron net-list
 neutron subnet-list
 
+echo ---Name resolution---
 cat /etc/resolv.conf
 cat /etc/hosts
+
+echo ---OpenStack status---
+/usr/bin/openstack-status
