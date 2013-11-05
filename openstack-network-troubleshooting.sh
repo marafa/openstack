056eef2e6090cfbf49e4ff540ce78f433405d363
root <root@openstack.marafa.vm>
2013-11-05 07:52:52 -0500

    beautify

--- openstack-network-troubleshooting.sh
+++ openstack-network-troubleshooting.sh
@@ -7,29 +7,37 @@ alias quantum=neutron
 
 echo "---Packages Installed---"
 rpm -qa | grep -iE "neutron|nova-network|openstack*" | sort
+echo 
 
 for i in `ls /etc/sysconfig/network-scripts/ifcfg-*`
 do
-	echo 
 	echo "---Device config for $i---"
 	cat $i
+	echo 
 done
 echo "---Ifconfig---"
 ifconfig
+echo 
 echo "---Routes----"
 route -n
+echo 
 echo ---Open vSwitch---
 ovs-vsctl show
+echo 
 echo ---Network Namespaces---
 ip netns
+echo 
 
 echo ---Neutron devices---
 neutron router-list
 for i in `neutron router-list|grep -v "id"|awk '{print $2}'`; do neutron router-show $i; done
+echo 
 neutron net-list
 for i in `neutron net-list|grep -v "id"| awk '{print $2}'`; do neutron net-show $i; done
+echo 
 neutron subnet-list
 for i in `neutron subnet-list|grep -v "id"|awk '{print $2}'`; do neutron subnet-show $i; done
+echo 
 
 echo ---Name resolution---
 cat /etc/resolv.conf
