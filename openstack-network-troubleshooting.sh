5fc1e16a67e38c9a3c2c4ac430b2a97221d5420a
root <root@openstack.marafa.vm>
2013-11-05 07:39:31 -0500

    drill down into neutron devices

--- openstack-network-troubleshooting.sh
+++ openstack-network-troubleshooting.sh
@@ -10,6 +10,7 @@ rpm -qa | grep -iE "neutron|nova-network|openstack*" | sort
 
 for i in `ls /etc/sysconfig/network-scripts/ifcfg-*`
 do
+	echo 
 	echo "---Device config for $i---"
 	cat $i
 done
@@ -24,8 +25,11 @@ ip netns
 
 echo ---Neutron devices---
 neutron router-list
+for i in `neutron router-list|grep -v "id"|awk '{print $2}'`; do neutron router-show $i; done
 neutron net-list
+for i in `neutron net-list|grep -v "id"| awk '{print $2}'`; do neutron net-show $i; done
 neutron subnet-list
+for i in `neutron subnet-list|grep -v "id"|awk '{print $2}'`; do neutron subnet-show $i; done
 
 echo ---Name resolution---
 cat /etc/resolv.conf
