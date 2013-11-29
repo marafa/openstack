8a063a89a82c7799e859009fda1f856f891d5584
marafa <root@openstack01.marafa.vm>
2013-11-29 17:02:02 -0500

    add syntax to rerun packstack

--- openstack-notes-working.sh
+++ openstack-notes-working.sh
@@ -6,8 +6,8 @@
 kvm=`virt-what`
 if [ "$kvm" == "kvm" ]
 then
-   sudo yum install -y mongodb-server mongodb
-   sudo sed -i '/--smallfiles/!s/OPTIONS=\"/OPTIONS=\"--smallfiles /' /etc/sysconfig/mongod
+   yum install -y mongodb-server mongodb
+   sed -i '/--smallfiles/!s/OPTIONS=\"/OPTIONS=\"--smallfiles /' /etc/sysconfig/mongod
 fi
 4.    packstack --allinone --nagios-install=n --mysql-pw=password --ntp-servers=0.pool.ntp.org,1.pool.ntp.org,2.pool.ntp.org --os-swift-install=y
 
@@ -17,7 +17,7 @@ before setting up a vm set up a network, firewall rules, ssh keys
 
 - network as DEMO user:
 
-i like to delete the networks made available with the allinone as they are not compatbile with my home network. so go to network topology tab on the left of the demo user's project panel and delete the public and private networks as well as the router to start fresh.
+i like to delete the networks made available with the allinone as they are not compatbile with my home network. so go to network topology tab on the left of the demo user''s project panel and delete the public and private networks as well as the router to start fresh.
 
 in the project tab on the left pane, select networks, press the create network button, enter the private network name (eg. demo_net) click on the subnet tab, enter demo_subnet fo rname and 10.20.30.0/24 . leave the gateway blank. create again, press the create network buton, enter the PUBLIC network name (eg. PublicNet) then public_subnet for the subnet name and YOUR network address eg. if the modem is 192.168.0.1 then put in 192.168.0.0/24. leave DHCP disabled. for the allocation pool pick a range that is sure to be free eg. 192.168.0.100, 192.168.0.200 .. dont leave out the comma. press create finally, create the router. call it router1
 
@@ -43,3 +43,6 @@ check your network topology. all 3 should be connected.
 next , modify variables then run https://raw.github.com/marafa/openstack/master/openstack-outside.sh . also remove /etc/sysconfig/network-scripts/ifcfg-br-ex before running that script.
 
 you are now ready to instantiate your instance
+
+if you need to rerun the installation follwing this syntax
+packstack -answer-file=packstack-answers
