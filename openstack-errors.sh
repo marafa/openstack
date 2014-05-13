04334c9ff25588e1acf70c30da97e72da7a52639
root <root@host02.marafa.vm>
2014-05-12 23:20:01 -0400

    first release

---
+++ openstack-errors.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]
+then
+	echo " Usage: $0 {warn|error|both}"
+	exit 1
+fi
+
+case $1 in
+	warn|Warn|WARN)
+		param=warn
+	;;
+	err|error|Error|ERROR|ERR)
+		param=error
+	;;
+	both)
+		param="warn|error"
+	;;
+esac
+
+grep -Eri --colour=auto $param /var/log/{nova,keystone,neutron,openvswitch,horizon,ceilometer,cinder,glance}
