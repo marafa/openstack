77da24c0df0836439821e8c4371737a805359a36
root <root@host02.marafa.vm>
2014-05-14 21:26:14 -0400

    added rhel7

--- openstack-images.sh
+++ openstack-images.sh
@@ -7,7 +7,7 @@ dir=/root/images.tmp
 md5file="`dirname $0`/glance-images.md5sum"
 
 usage(){
-        echo " Usage: `basename $0` all centos cirros fedora19"
+        echo " Usage: `basename $0` all centos cirros fedora19 centos65 rhel7"
         echo
         echo " Submit image locations at https://github.com/marafa/openstack"
         exit 1
@@ -34,6 +34,20 @@ centos(){
         images
 }
 
+centos65(){
+	location=http://repos.fedorapeople.org/repos/openstack/guest-images/centos-6.5-20140117.0.x86_64.qcow2
+	image=centos-6.5-20140117.0.x86_64.qcow2
+	name="CentOS 6.5 x86_64"
+	images
+}
+
+rhel7(){
+	location=ftp://ftp.redhat.com/redhat/rhel/rc/7/GuestImage/rhel-guest-image-7.0-20140410.0.x86_64.qcow2
+	image=rhel-guest-image-7.0-20140410.0.x86_64.qcow2
+	name="RHEL 7.0 x86_64"
+	images
+}
+
 download(){
 if ! [ -a $dir/$image ]
 then
@@ -54,13 +68,13 @@ get_md5sum(){
 #if [ -f $dir/glance-images.md5sum ]
 if ! [ -f $md5file ]
 then
-		echo " INFO: Downloading md5sums to $md5file"
+	echo " INFO: Downloading md5sums to $md5file"
         wget https://raw.github.com/marafa/openstack/master/glance-images.md5sum -O $md5file
 fi
 }
 
 images(){
-debug
+#debug
 get_md5sum
 download
 echo " INFO: Checking md5sum of $image"
@@ -81,6 +95,8 @@ all(){
         cirros
         fedora19
         centos
+        centos65
+	rhel7
 }
 
 ####main
@@ -110,6 +126,12 @@ case $1 in
         all)
                 all
         ;;
+	centos65)
+		centos65
+	;;
+	rhel7)
+		rhel7
+	;;	
         *)
                 usage
         ;;
