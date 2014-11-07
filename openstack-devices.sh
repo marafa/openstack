c95adacda537ece511e6fd0506e8018c7d19fb00
root <root@host02.marafa.vm>
2014-11-06 21:07:29 -0500

    improvements

--- openstack-devices.sh
+++ openstack-devices.sh
@@ -6,9 +6,9 @@ debug(){
 echo -----debug	-----
 echo files $files 
 echo file $file 
-echo nics $nics
+echo num_nics $num_nics
 echo nic $nic
-echo device $device
+echo device=$device
 echo DEVICE=$DEVICE
 echo IPADDR=$IPADDR
 echo HWADDR=$HWADDR
@@ -25,41 +25,43 @@ then
 	exit 0
 fi
 
-	nics=`ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v lo |wc -l`
-	if [ $nics -gt 1 ]
-	then
-		files=`ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v lo`
-		PS3="Select primary network interface: "
-		select nic in $files quit
-		do
-		case nic in 
-		quit)
-			echo quitting
-			exit 1
-		;;
-		*)
-			nic=`basename $nic`
-			file=/etc/sysconfig/network-scripts/$nic
-#debug
-		;;
-		esac
-		break
-		done
-	else
-		file=`ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v lo`
-	fi
-	mkdir -p ~/backups
-	cp -n $file ~/backups/.
-	cp -n ~/backups/$nic /etc/sysconfig/network-scripts/ifcfg-br-ex
-	device=`basename $file`
-	IPADDR=`grep IPADDR ~/backups/$nic|cut -d= -f2`	
-	GATEWAY=`grep GATEWAY ~/backups/$nic|cut -d= -f2`
-	DNS1=`grep DNS1 ~/backups/$nic|cut -d= -f2`
-	DNS2=`grep DNS2 ~/backups/$nic|cut -d= -f2`
+#number of nics on the server
+num_nics=`ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v lo |wc -l`
+if [ $num_nics -gt 1 ] 
+then
+	files=`ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v lo`
+	PS3="Select primary network interface: "
+	select file in $files quit
+	do
+	case file in 
+	quit)
+		echo Quitting
+		exit 1
+	;;
+	*)
+		nic=`basename $file|cut -d- -f2`
+		file=/etc/sysconfig/network-scripts/ifcfg-$nic
+	;;
+	esac
+	break
+	done
+else
+	file=`ls /etc/sysconfig/network-scripts/ifcfg-* |grep -v lo`
+fi
+
+primary(){
+mkdir -p ~/backups
+cp -n $file ~/backups/.
+device=`basename $file`
+nic=`basename $device|cut -d- -f2`
+IPADDR=`grep IPADDR ~/backups/$device|cut -d= -f2`	
+GATEWAY=`grep GATEWAY ~/backups/$device|cut -d= -f2`
+DNS1=`grep DNS1 ~/backups/$device|cut -d= -f2`
+DNS2=`grep DNS2 ~/backups/$device|cut -d= -f2`
 
 ##configuration of $device
 DEVICE=`echo $nic|cut -d- -f2`
-HWADDR=`cat ~/backups/$nic|grep HWADDR|cut -d= -f2`
+HWADDR=`cat ~/backups/$device|grep HWADDR|cut -d= -f2`
 rm -rf $file
 cat >> $file << EOF
 DEVICE=$DEVICE
@@ -67,7 +69,9 @@ HWADDR=$HWADDR
 TYPE=Ethernet
 ONBOOT=yes
 EOF
+}
 
+br_ext(){
 ##configuration of br-ex
 file=/etc/sysconfig/network-scripts/ifcfg-br-ex
 rm -rf $file
@@ -81,14 +85,22 @@ DNS2=$DNS2
 HOSTNAME=`hostname`
 ONBOOT=yes
 EOF
+}
 
+ovs(){
 ##configuration of ovs
-ovs-vsctl show |grep $device > /dev/null
+ovs-vsctl show |grep $nic > /dev/null
 result=$?
 if ! [ $result -eq 0 ]
 then
-	ovs-vsctl add-port br-ex $device
+	ovs-vsctl add-port br-ex $nic
 	service network restart
 fi
+}
+
+primary #primary nic aka eth0
+br_ext
+ovs
 
 #debug
+
