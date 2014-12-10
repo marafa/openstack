2ffea2302f8cb0459f8f616b1cc63fa9a300072e
root <root@host02.marafa.vm>
2014-12-10 14:28:55 -0500

    add parameters to collect or follow the errors

--- openstack-errors.sh
+++ openstack-errors.sh
@@ -4,6 +4,11 @@ if [ $# -eq 0 ]
 then
 	echo " Usage: $0 {warn|error|both}"
 	exit 1
+elif [ $# -eq 1 ]
+then
+	export task="c"
+else
+	task=$2
 fi
 
 case $1 in
@@ -18,6 +23,8 @@ case $1 in
 	;;
 esac
 
+
+
 follow(){
 multitail -E "$param" -Evi "info" --mergeall /var/log/{nova,keystone,neutron,openvswitch,horizon,ceilometer,cinder,glance}/*
 }
@@ -26,7 +33,7 @@ collect(){
 grep -Eri --colour=auto $param /var/log/{nova,keystone,neutron,openvswitch,horizon,ceilometer,cinder,glance}
 }
 
-case $2 in 
+case $task in 
 	f)
 		follow
 	;;
