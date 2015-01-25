1296ecd8f01abef5f30be1d92bc927927d77b615
root <root@os.echognumix.vm>
2015-01-25 01:01:25 -0500

    update for icehouse

--- openstack-project-add.sh
+++ openstack-project-add.sh
@@ -43,19 +43,28 @@ echo "------- check if password $password has special characters that might brea
 
 assign_role_to_user(){
 #keystone user-role-add --user $user$id --role admin$id --tenant $project$id
-keystone user-role-add --user $user$id --role Member --tenant $project$id
+keystone user-role-add --user $user$id --role _member_ --tenant $project$id
 }
 
 create_networks(){
-source $ks_dir/keystonerc_$user$id
+if [ -f $ks_dir/keystonerc_$user$id ]
+then
+	source $ks_dir/keystonerc_$user$id
+else
+	echo "FAIL: $ks_dir/keystonerc_$user$id not found!"
+	exit 6
+fi
 
 neutron router-create router$id
 neutron net-create PrivateNet_$id
+source /root/keystonerc_admin
 CIDR=`neutron subnet-list | awk '{print $6}'| grep ^10. | cut -d/ -f1`
+source $ks_dir/keystonerc_$user$id
 if [ "$CIDR" == "" ]
 then
 	echo "WARN: CIDR 10.x.x.x not found. Is this a new install?"
 	CIDR=10.0.0.0
+	echo " INFO: Using CIDR $CIDR"
 fi
 CIDR2=`echo $CIDR| cut -d. -f2`
 CIDR3=`echo $CIDR| cut -d. -f3`
@@ -74,31 +83,35 @@ fi
 
 CIDR=10.$CIDR2.$CIDR3.0
 
-neutron subnet-create --dns-nameservers 8.8.8.8 8.8.8.7 --name PrivateSubnet_$id PrivateNet_$id $CIDR/24
+neutron subnet-create PrivateNet_$id $CIDR/24 --dns-nameserver 8.8.8.8 --dns-nameserver 8.8.8.7 --name PrivateSubnet_$id
 neutron router-interface-add router$id PrivateSubnet_$id
 
 source /root/keystonerc_admin
-#neutron router-gateway-set router$id PublicLAN ##why? this is already done in openstack-outside.sh
+neutron router-gateway-set router$id public ##why? this is already done in openstack-outside.sh
 }
 
 keystonerc(){
 echo " INFO: Writing $ks_dir/keystonerc_$user$id"
+ipaddr=`ip addr show br-ex |grep inet -w|awk '{print $2}' |sed 's,/24,,g'`
 mkdir -p $ks_dir
 cat >> $ks_dir/keystonerc_$user$id << EOF
 export OS_USERNAME=$user$id
 export OS_TENANT_NAME=$project$id
 export OS_PASSWORD=$password
-export OS_AUTH_URL=http://192.168.0.33:35357/v2.0/
+export OS_AUTH_URL=http://$ipaddr:35357/v2.0/
 export PS1='[\u@\h \W(\033[1;32mkeystone_$user$id\033[0m)]\$ '
 EOF
 }
 
 write_security_rules(){
-echo use neutron secgroup to add ssh and ping rules
+echo "todo: use neutron secgroup to add ssh and ping rules instead of nova"
 source $ks_dir/keystonerc_$user$id
 nova keypair-add key$id > $ks_dir/key$id.pem
 nova secgroup-create SecGrp$id "Security Group $id"
 nova secgroup-add-rule SecGrp$id tcp 22 22 0.0.0.0/0
+neutron security-group-rule-create --direction ingress --protocol tcp --port_range_min 1 --port_range_max 65535 SecGrp$id 
+neutron security-group-rule-create --direction ingress --protocol udp --port_range_min 1 --port_range_max 65535 SecGrp$id 
+neutron security-group-rule-create --direction ingress --protocol icmp SecGrp$id
 }
 
 ###main
--- openstack-project-delete.sh
+++ openstack-project-delete.sh
@@ -56,11 +56,23 @@ echo " INFO: Deleting $ks_dir/keystonerc_$user$id"
 rm -rf $ks_dir/keystonerc_$user$id
 }
 
+pem_file(){
+echo " INFO: Deleting $ks_dir/$user$id.pem "
+rm -rf $ks_dir/key$id.pem
+}
+
+keypair(){
+echo " INFO: Deleting keypair key$id"
+nova keypair-delete key$id
+}
+
 network_router
 network_net
 network_subnet
 secgroup
+keypair
 user
 role
 tenant
 keystonerc
+pem_file
